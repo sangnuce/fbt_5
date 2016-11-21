3db452f3daac817880aced82b7b592dcdeb03499
Luong Van Sang <sanga2k50@gmail.com>
2016-11-22 05:24:32 +0700

    Show tours homepage - 2

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,9 @@
 class Place < ApplicationRecord
+  enum place_type: {region: 0, city: 1, place: 2}
+
   has_closure_tree
 
   has_many :tour_places
+
+  scope :popular_desc, ->{left_joins(:tour_places).group(:id).order "COUNT(tour_id) DESC"}
 end
