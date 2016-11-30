348a2ffb058cf5cc2d0c61cfcf776f1bd9b4aebe
Nguyen Huyen <nguyen.my.huyen@framgia.com>
2016-11-30 11:10:01 +0700

    Admin show category + fix show place

--- app/helpers/places_helper.rb
+++ app/helpers/places_helper.rb
@@ -1,9 +1,12 @@
 module PlacesHelper
   def places_tree_for places
     places.map do |place, nested_places|
-      render(place) + (nested_places.size > 0 ? content_tag(:tr,
-        places_tree_for(nested_places), class: "panel-collapse collapse",
-        id: "place-#{place.place_type}") : nil)
+      html = ""
+      if place.present?
+        html << content_tag(:tr, render(place),
+          class: place.root? ? "" : "panel-collapse collapse
+          place-#{place.parent_id}") << places_tree_for(nested_places)
+      end
     end.join.html_safe
   end
 end
