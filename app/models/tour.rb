57350f3433d0db746c5686d96f73bd93b8e774d7
Luong Van Sang <sanga2k50@gmail.com>
2016-11-30 14:47:21 +0700

    [Admin] Update tour discount

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -25,4 +25,7 @@ class Tour < ApplicationRecord
   end
   scope :rating_order, ->rule {order(rating: rule) if rule.present?}
   scope :price_order, ->rule {order(price_per_person: rule) if rule.present?}
+
+  validates :discount, presence: true,
+    numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 100}
 end
